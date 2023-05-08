edefcc9355b99f97a6349d044aa226fe90f80061
Pepe Duran <Pepe Duran@192.168.1.130>
2023-05-08 22:11:46 +0200

    Subida de rama funciona

--- src/main/java/es/tragsatec/pssa/mapper/AnimalesMapper.java
+++ src/main/java/es/tragsatec/pssa/mapper/AnimalesMapper.java
@@ -8,6 +8,28 @@ import org.apache.ibatis.annotations.Param;
 public interface AnimalesMapper {
     long countByExample(AnimalesExample example);
 
+	int deleteByExample(AnimalesExample example);
+
+	int deleteByPrimaryKey(Integer id);
+
+	int insert(Animales row);
+
+	int insertSelective(Animales row);
+
+	List<Animales> selectByExample(AnimalesExample example);
+
+	Animales selectByPrimaryKey(Integer id);
+
+	int updateByExampleSelective(@Param("row") Animales row, @Param("example") AnimalesExample example);
+
+	int updateByExample(@Param("row") Animales row, @Param("example") AnimalesExample example);
+
+	int updateByPrimaryKeySelective(Animales row);
+
+	int updateByPrimaryKey(Animales row);
+
+	long countByExample(AnimalesExample example);
+
     int deleteByExample(AnimalesExample example);
 
     int deleteByPrimaryKey(Integer id);
--- src/main/java/es/tragsatec/pssa/model/Animales.java
+++ src/main/java/es/tragsatec/pssa/model/Animales.java
@@ -3,6 +3,216 @@ package es.tragsatec.pssa.model;
 public class Animales {
     private Integer id;
 
+	private String crotal;
+
+	private String fechainicio;
+
+	private String fechafin;
+
+	private String explotacionpertenencia;
+
+	private String explotacionubicacion;
+
+	private String fechanacimiento;
+
+	private String explotacionnacimiento;
+
+	private String sexo;
+
+	private String raza;
+
+	private String crotalmadre;
+
+	private String causaalta;
+
+	private String fechaalta;
+
+	private String procedencia;
+
+	private String codigolocalentrada;
+
+	private String codigoremoentrada;
+
+	private String causabaja;
+
+	private String fechabaja;
+
+	private String destino;
+
+	private String codigolocalsalida;
+
+	private String codigoremosalida;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCrotal() {
+		return crotal;
+	}
+
+	public void setCrotal(String crotal) {
+		this.crotal = crotal;
+	}
+
+	public String getFechainicio() {
+		return fechainicio;
+	}
+
+	public void setFechainicio(String fechainicio) {
+		this.fechainicio = fechainicio;
+	}
+
+	public String getFechafin() {
+		return fechafin;
+	}
+
+	public void setFechafin(String fechafin) {
+		this.fechafin = fechafin;
+	}
+
+	public String getExplotacionpertenencia() {
+		return explotacionpertenencia;
+	}
+
+	public void setExplotacionpertenencia(String explotacionpertenencia) {
+		this.explotacionpertenencia = explotacionpertenencia;
+	}
+
+	public String getExplotacionubicacion() {
+		return explotacionubicacion;
+	}
+
+	public void setExplotacionubicacion(String explotacionubicacion) {
+		this.explotacionubicacion = explotacionubicacion;
+	}
+
+	public String getFechanacimiento() {
+		return fechanacimiento;
+	}
+
+	public void setFechanacimiento(String fechanacimiento) {
+		this.fechanacimiento = fechanacimiento;
+	}
+
+	public String getExplotacionnacimiento() {
+		return explotacionnacimiento;
+	}
+
+	public void setExplotacionnacimiento(String explotacionnacimiento) {
+		this.explotacionnacimiento = explotacionnacimiento;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
+	public String getCrotalmadre() {
+		return crotalmadre;
+	}
+
+	public void setCrotalmadre(String crotalmadre) {
+		this.crotalmadre = crotalmadre;
+	}
+
+	public String getCausaalta() {
+		return causaalta;
+	}
+
+	public void setCausaalta(String causaalta) {
+		this.causaalta = causaalta;
+	}
+
+	public String getFechaalta() {
+		return fechaalta;
+	}
+
+	public void setFechaalta(String fechaalta) {
+		this.fechaalta = fechaalta;
+	}
+
+	public String getProcedencia() {
+		return procedencia;
+	}
+
+	public void setProcedencia(String procedencia) {
+		this.procedencia = procedencia;
+	}
+
+	public String getCodigolocalentrada() {
+		return codigolocalentrada;
+	}
+
+	public void setCodigolocalentrada(String codigolocalentrada) {
+		this.codigolocalentrada = codigolocalentrada;
+	}
+
+	public String getCodigoremoentrada() {
+		return codigoremoentrada;
+	}
+
+	public void setCodigoremoentrada(String codigoremoentrada) {
+		this.codigoremoentrada = codigoremoentrada;
+	}
+
+	public String getCausabaja() {
+		return causabaja;
+	}
+
+	public void setCausabaja(String causabaja) {
+		this.causabaja = causabaja;
+	}
+
+	public String getFechabaja() {
+		return fechabaja;
+	}
+
+	public void setFechabaja(String fechabaja) {
+		this.fechabaja = fechabaja;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public String getCodigolocalsalida() {
+		return codigolocalsalida;
+	}
+
+	public void setCodigolocalsalida(String codigolocalsalida) {
+		this.codigolocalsalida = codigolocalsalida;
+	}
+
+	public String getCodigoremosalida() {
+		return codigoremosalida;
+	}
+
+	public void setCodigoremosalida(String codigoremosalida) {
+		this.codigoremosalida = codigoremosalida;
+	}
+
+	private Integer id;
+
     private String crotal;
 
     private String fechainicio;
